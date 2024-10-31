2d303160fc6e3f3a8285a8c04db201b115c3121b
Miguel A. Ortiz Acuna <miangeltiz20@gmail.com>
2024-10-31 10:13:38 -0300

    teste de verdade 8000 epocas

--- train_CTGAN.py
+++ train_CTGAN.py
@@ -11,7 +11,7 @@ def main():
     trainer.visualize_metadata() 
 
     trainer.train_synthesizer(
-        epochs=1,
+        epochs=8000,
         cuda=True,
         enforce_rounding=True,
         verbose=True
